53ced451a9f2c4a8dd1386f91e07ce5831a5f5c6
Fra_592 <benji.f592@gmail.com>
2017-01-12 16:34:02 +0100

    Récupération des images - 3

--- io.c
+++ io.c
@@ -1,71 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #include "BMP/bmp_worker.h"
 #include "io.h"
 
-#define DEBUG
-
-int main(void)
+int **get_image_pixels(const char *chemin, int **image)
 {
-  	// Ouverture du fichier
-	FILE * fBMP = fopen("/home/fra_592/Documents/Cours/2017/Projet_Algo/BMP/img/test_32x32.bmp", "rb+");
-	if(fBMP == NULL)
-	{
-		#ifdef DEBUG
-			puts("Can't open the BMP file");
-		#endif
-		return(ERROR_OPENING_FILE);
-	}
-	#ifdef DEBUG
-    	puts("File opened.");
-	#endif
-
-	// Lecture des Headers
-    BmpWorker_fileHeader * fileHeader = (BmpWorker_fileHeader *) malloc(sizeof(BmpWorker_fileHeader));
-    BmpWorker_infoHeader * bmpHeader = (BmpWorker_infoHeader *) malloc(sizeof(BmpWorker_infoHeader));
-    
-    if(BmpWorker_FileHeader_read(fBMP, fileHeader) != SUCCESS)
-    {
-    	#ifdef DEBUG
-			puts("Can't read file header.");
-		#endif
-    	return(ERROR_INVALID_FILE_HEADER);
-    }
-    #ifdef DEBUG
-    	puts("File header opened.");
-	#endif
-
-	if(BmpWorker_InfoHeader_read(fBMP, bmpHeader) != SUCCESS)
-	{
-		#ifdef DEBUG
-			puts("Can't read info header.");
-		#endif
-		return(ERROR_INVALID_INFO_HEADER);
-	}
-	#ifdef DEBUG
-    	puts("Info header opened.");
-	#endif
-
-    uint32_t width = bmpHeader->img_width;
-    uint32_t height = bmpHeader->img_height;
-    uint8_t * pData = (uint8_t *) malloc(bmpHeader->bmp_bytesz * sizeof(uint8_t));
+	//Ouverture du fichier
+	FILE *fichier = fopen(chemin, "rb+");
+	assert(fichier != NULL);
 	
 	#ifdef DEBUG
-		printf("W: %i, H: %i, TS: %i, RS: %i\n", width, height, width * height, bmpHeader->bmp_bytesz);
+	if(fichier == NULL)
+	{
+			puts("Impossible d'ouvrir ce fichier.");
+	}
+	puts("Fichier ouvert avec succès.");
 	#endif
 
 
-	// Lecture des données
-    if(BmpWorker_RawData_read(fBMP, fileHeader, bmpHeader, pData) != SUCCESS)
-    {
-    	#ifdef DEBUG
-    		puts("Can't read bitmap data.");
-		#endif
-    	return(ERROR_INVALID_DATA);
-    }
+	//Lecture des headers
+    BmpWorker_fileHeader *fileHeader = (BmpWorker_fileHeader *) malloc(sizeof(BmpWorker_fileHeader));
+	
+	#ifdef DEBUG
+	if(BmpWorker_FileHeader_read(fichier, fileHeader) != SUCCESS)
+	{
+		puts("Impossible d'ouvrir le header du fichier.");
+	}
+  	puts("Header du fichier ouvert lu avec succès.");
+	#endif
 
+	BmpWorker_infoHeader * imgHeader = (BmpWorker_infoHeader *) malloc(sizeof(BmpWorker_infoHeader));
+	
+	#ifdef DEBUG
+	if(BmpWorker_InfoHeader_read(fichier, imgHeader) != SUCCESS)
+	{
+		puts("Impossible d'ouvrir le header BMP.");
+	}
+  	puts("Header BMP ouvert lu avec succès.");
+	#endif
+
+    uint32_t width = imgHeader->img_width;
+    uint32_t height = imgHeader->img_height;
+
+	#ifdef DEBUG
+		printf("W: %i, H: %i, TS: %i, RS: %i\n", width, height, width * height, imgHeader->bmp_bytesz);
+	#endif
+
+
+	//Lecture des données de l'image
+    uint8_t * pData = (uint8_t *) malloc(imgHeader->bmp_bytesz * sizeof(uint8_t));
+
+    #ifdef DEBUG
+    if(BmpWorker_RawData_read(fichier, fileHeader, imgHeader, pData) != SUCCESS)
+    {
+    	puts("Impossible de lire les données de l'image.");
+    }
+	#endif
+
+
+    // Conversion
     int i, j;
     int image[height][width];
 
-    // Conversion
     for(i = 0; i < height; i++)
     {
     	for(j = 0; j < width; j++)
@@ -74,4 +72,16 @@ int main(void)
     		printf("%d\n", image[i][j]);
     	}
     }
+
+
+	fclose(fichier);
+	return(image);
+}
+
+int main(void)
+{
+	int **image;
+	image = (int *) malloc()
+
+	image = get_image_pixels("/home/fra_592/Documents/Cours/2017/Projet_Algo/BMP/img/test_32x32.bmp");
 }
